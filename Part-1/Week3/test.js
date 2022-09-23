49bc64d18253a1b55e281d21668703db9c2f39c7
Mariessa Pinto <mpinto13@my.bcit.ca>
2022-09-23 11:37:38 -0700

    fixed missing test file content

---
+++ Part-1/Week3/test.js
@@ -0,0 +1,22 @@
+// This is a single line
+
+/**
+ * 
+ * This is one line
+ * This is another
+ * 
+ */
+
+ var num = myFunction(2,3);
+
+ /**
+  * @desc This will calculate the area
+  * @param {*} x is the width
+  * @param {*} y is the height
+  * @returns the area of the object
+  */
+ function myFunction(x,y){
+     return x * y;
+ };
+ 
+ console.log(num);
